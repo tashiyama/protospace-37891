33291ae5a9d103263f8147f484ad808d8a15fe58
tashiyama <mototo8201@gmail.com>
2022-05-03 01:14:13 +0900

    :concept ,入れ忘れ

--- db/migrate/20220429093134_create_prototypes.rb
+++ db/migrate/20220429093134_create_prototypes.rb
@@ -3,7 +3,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
     create_table :prototypes do |t|
       t.string     :title,      null: false
       t.text       :catch_copy, null: false
-      t.text       :concept     null: false
+      t.text       :concept,     null: false
       t.references :user,       null: false, foreign_key: true
       t.timestamps
     end
